8a4172afb18605c57c6ec6b07dce119c098ff4ec
Vinicius da Mata Pickrodt <vinipickrodt@gmail.com>
2019-06-10 00:17:05 -0300

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        static Random random = new Random(42);
+        static int bomba = random.Next(1, 4);
+        static int score = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void ButtonClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            btn1.BackgroundColor = btn2.BackgroundColor = btn3.BackgroundColor = new Button().BackgroundColor;
+
+            if (button.Text == bomba.ToString())
+            {
+                button.BackgroundColor = Color.Red;
+                var t = Android.Widget.Toast.MakeText(Android.App.Application.Context, $"Bomba explodiu. \nSua pontuação foi {score}.", Android.Widget.ToastLength.Short);
+                score = 0;
+                t.Show();
+                //await DisplayAlert($"GAME OVER", $"Bomba explodiu. \nSua pontuação foi {score}", "Tentar de novo");
+            }
+            else
+            {
+                button.BackgroundColor = Color.Yellow;
+                score++;
+                //await DisplayAlert("Parabéns", $"Bomba desarmada! Sua pontuação é {score}.", "Continuar");
+            }
+
+            lblScore.Text = "Pontuação: " + score.ToString();
+
+            bomba = random.Next(1, 4);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A().Wait();
+        }
+
+        static async Task A()
+        {
+            ClientWebSocket clientWebSocket = new ClientWebSocket();
+            var uri = new Uri("wss://www.bitmex.com/realtime?subscribe=quote,XBTUSD");
+            await clientWebSocket.ConnectAsync(uri, CancellationToken.None);
+
+            do
+            {
+                var str = await Read(clientWebSocket);
+                Console.WriteLine(str);
+            } while (true);
+        }
+
+        private static async Task<string> Read(ClientWebSocket clientWebSocket)
+        {
+            var allMsgs = new List<string>();
+            WebSocketReceiveResult r = null;
+
+            do
+            {
+                var msgData = new byte[1024 * 200];
+                var msg = new ArraySegment<byte>(msgData);
+
+                r = await clientWebSocket.ReceiveAsync(msg, CancellationToken.None);
+
+                var msgStream = new StreamReader(new MemoryStream(msgData));
+                var msgStr = msgStream.ReadToEnd();
+
+                allMsgs.Add(msgStr);
+            } while (!r.EndOfMessage);
+
+            return string.Join("", allMsgs);
+        }
+    }
+}
